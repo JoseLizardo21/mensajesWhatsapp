901dc60b03805c1f39c6bb413cca20d6b1e1c69e
Jose <jose.212002lizardo@gmail.com>
2024-06-17 18:21:43 -0500

    Update library

--- src/lib/whatsapp.js
+++ src/lib/whatsapp.js
@@ -5,7 +5,12 @@ const whatsapp = new Client({
   puppeteer: {
 		args: ['--no-sandbox', '--disable-setuid-sandbox'],
 	},
-  authStrategy: new LocalAuth()
+  authStrategy: new LocalAuth(),
+  webVersion: '2.2412.54',
+  webVersionCache: {
+    type: 'remote',
+    remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+  },
 });
 
 whatsapp.on('qr', qr => {
